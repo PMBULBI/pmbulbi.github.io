373ff2040b63a7adf8bbb0206201231e88247760
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-24 12:47:21 +0700

    update(whatsauth):0.1.1

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
-import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.0.9/whatsauth.js";
-import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.0.9/config.js";
+import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.1/whatsauth.js";
+import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.1/config.js";
 
 
 deleteCookie(wauthparam.tokencookiename);
