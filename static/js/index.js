add0cab5a8738d634c5eae81e991dc866d9b087e
JPratama7 <josepratama080@gmail.com>
2023-10-24 14:27:29 +0700

    fix(index.js): update keyword ke iteung

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,6 @@
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.2/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.2/config.js";
 
-
+wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgwLmtleS4=";
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
