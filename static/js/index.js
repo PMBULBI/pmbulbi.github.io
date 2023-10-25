3750bd255654c1e57d0e30dfcaff6c75a3d390d8
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-25 17:19:34 +0700

    Update(index.js):versi whatsauth 1.4

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
-import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.3/whatsauth.js";
-import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.3/config.js";
+import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.4/whatsauth.js";
+import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.4/config.js";
 
 wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgwLmtleS4=";
 deleteCookie(wauthparam.tokencookiename);
