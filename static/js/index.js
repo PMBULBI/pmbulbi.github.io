6bd38bc83f7f0ae164f854e00134d5c2462585a2
Rolly Maulana Awangga <awangga@gmail.com>
2023-10-27 15:45:46 +0700

    Update(index.js):naik versi 1.6 whatsauth

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
-import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.5/whatsauth.js";
-import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.5/config.js";
+import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.6/whatsauth.js";
+import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.6/config.js";
 
 wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgwLmtleS4=";
 deleteCookie(wauthparam.tokencookiename);
