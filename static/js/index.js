7b53fff19e26d087daf806a5fb8035638e3618d4
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-10-30 09:57:12 +0700

    style(login) : rapihin code

--- static/js/index.js
+++ static/js/index.js
@@ -1,6 +1,7 @@
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.4/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.4/config.js";
 
+// Untuk Login QR Code
 wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgwLmtleS4=";
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
