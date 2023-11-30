2189272075b96e88050ba4a6025f50f32868f68d
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-30 14:20:12 +0700

    feat(login) : create function login with cihuy

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,44 @@
+import {
+    CihuyDataAPI,
+    CihuyPostApi,
+    CihuyDeleteAPI,
+    CihuyUpdateApi,
+  } from "https://c-craftjs.github.io/lulu/api.js";
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.6/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.6/config.js";
+import { UrlLoginMhsEmail } from "./controller/template";
+import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
+
+export const token = CihuyGetCookie("login");
 
 // Untuk Login QR Code
 wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgwLmtleS4=";
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
+
+// Untuk Login via Email
+const apiUrlLoginMhsEmail = UrlLoginMhsEmail
+
+CihuyPostApi(apiUrlLoginMhsEmail, token, data)
+  .then((responseText) => {
+    console.log("Respon sukses : ", responseText);
+    // Menampilkan pesan sukses
+    Swal.fire({
+        icon: "success",
+        title: "Sukses!",
+        text: "Berhasil masuk ke PMB Mahasiswa.",
+        showConfirmButton: false,
+        timer: 1500,
+    }).then(() => {
+        // Refresh halaman atau tindakan lain jika diperlukan
+        window.location.href = 'https://pmb.ulbi.ac.id/pmb-mhs';
+    });
+  })
+  .catch((error) => {
+    console.error("Terjadi kesalahan : ", error);
+    Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Terjadi kesalahan saat login akun.",
+    });
+});
