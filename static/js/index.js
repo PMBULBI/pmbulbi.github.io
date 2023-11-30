9ff7d9adda158a82f60f897e7c9f60bc27400e3e
Bachtiar21 <bachtiarramadhan26@gmail.com>
2023-11-30 14:32:54 +0700

    feat(login) : coba login mahasiswa function

--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,7 @@
-import {
-    CihuyDataAPI,
-    CihuyPostApi,
-    CihuyDeleteAPI,
-    CihuyUpdateApi,
-  } from "https://c-craftjs.github.io/lulu/api.js";
+import { CihuyPostApi } from "https://c-craftjs.github.io/lulu/api.js";
 import {qrController,deleteCookie} from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.6/whatsauth.js";
 import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.1.6/config.js";
-import { UrlLoginMhsEmail } from "./controller/template";
+import { UrlLoginMhsEmail } from "./controller/template.js";
 import { CihuyGetCookie } from "https://c-craftjs.github.io/cookies/cookies.js";
 
 export const token = CihuyGetCookie("login");
@@ -16,29 +11,42 @@ wauthparam.keyword = "aHR0cHM6Ly93YS5tZS82MjgxMTIwMDAyNzk/dGV4dD13aDR0NWF1dGgwLm
 deleteCookie(wauthparam.tokencookiename);
 qrController(wauthparam);
 
-// Untuk Login via Email
-const apiUrlLoginMhsEmail = UrlLoginMhsEmail
+// Login Mahasiswa with Email & Password
+document.getElementById("masukButton").addEventListener("click", function () {
+    // Untuk ambil nilai dari form
+    const email = document.getElementById("Email").value;
+    const password = document.getElementById("Password").value;
 
-CihuyPostApi(apiUrlLoginMhsEmail, token, data)
-  .then((responseText) => {
-    console.log("Respon sukses : ", responseText);
-    // Menampilkan pesan sukses
-    Swal.fire({
-        icon: "success",
-        title: "Sukses!",
-        text: "Berhasil masuk ke PMB Mahasiswa.",
-        showConfirmButton: false,
-        timer: 1500,
-    }).then(() => {
-        // Refresh halaman atau tindakan lain jika diperlukan
-        window.location.href = 'https://pmb.ulbi.ac.id/pmb-mhs';
+    // Buat Data untuk postnya
+    const data = {
+        email: email,
+        password: password
+    };
+
+    // Untuk Login via Email
+    const apiUrlLoginMhsEmail = UrlLoginMhsEmail
+
+    CihuyPostApi(apiUrlLoginMhsEmail, token, data)
+    .then((responseText) => {
+        console.log("Respon sukses : ", responseText);
+        // Menampilkan pesan sukses
+        Swal.fire({
+            icon: "success",
+            title: "Sukses!",
+            text: "Berhasil masuk ke PMB Mahasiswa.",
+            showConfirmButton: false,
+            timer: 1500,
+        }).then(() => {
+            // Refresh halaman atau tindakan lain jika diperlukan
+            window.location.href = 'https://pmb.ulbi.ac.id/pmb-mhs';
+        });
+    })
+    .catch((error) => {
+        console.error("Terjadi kesalahan : ", error);
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Terjadi kesalahan saat login akun.",
+        });
     });
-  })
-  .catch((error) => {
-    console.error("Terjadi kesalahan : ", error);
-    Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Terjadi kesalahan saat login akun.",
-    });
-});
+})
